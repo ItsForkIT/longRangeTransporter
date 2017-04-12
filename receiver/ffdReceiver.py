7d022a2964b3e5d39ce2e0753614d592659351b9
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-04-12 20:05:30 +0530

    Bug fixes

--- receiver/ffdReceiver.py
+++ receiver/ffdReceiver.py
@@ -9,8 +9,14 @@ else:
 
 with serial.Serial(interface, 9600, timeout=3600) as ser:
     stream = b''
+    count = 0
     while 1:
-        x = ser.read()
+        x = ser.read(1)
+        print(x)
+        count += 1
+        if not (count % 80):
+            print("\n==========\n")
+        continue
         if x == b'\x00':
             message = stream.decode("utf-8")
             print("RECEIVED: ", message)
