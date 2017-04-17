cc1f0add3470da99ea4c2d4653d5db5fff6c0d65
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-04-17 19:44:45 +0530

    Improve receiver

--- receiver/ffdReceiver.py
+++ receiver/ffdReceiver.py
@@ -1,24 +1,53 @@
 import serial
 import sys
+import os
+
+# PACKET TYPES
+INITIATION = b'\x01\x00'
+DATA = b'\x02\x00'
+TERMINATION = b'\x03\x00'
+
 
 if len(sys.argv) > 1:
     interface = sys.argv[1]
 else:
     interface = '/dev/ttyUSB0'
 
+if len(sys.argv) > 2:
+    directory = sys.argv[2]
+else:
+    directory = ''
+
+
+fileObj = None
 
 with serial.Serial(interface, 9600, timeout=3600) as ser:
-    stream = b''
-    count = 0
     while 1:
-        x = ser.read(1)
-        print(x)
-        count += 1
-        if not (count % 80):
-            print("\n==========\n")
-        continue
-        if x == b'\x00':
-            message = stream.decode("utf-8")
-            print("RECEIVED: ", message)
+            print("Receiving Packet .. ")
             stream = b''
-        stream += x          # read one byte
+            count = 0
+
+            msg_type = ser.read(2)
+            print(msg_type)
+
+            msg_len = ser.read(2)
+            print(msg_len)
+            msg_len = int.from_bytes(msg_len, byteorder='little')
+            print(msg_len)
+
+            while count < 76:
+                x = ser.read(1)
+                count += 1
+                stream += x
+
+            stream = stream[:msg_len]
+
+            if (not fileObj) and msg_type == INITIATION:
+                fileObj = open(os.path.join(directory, stream.decode()), 'wb')
+            elif fileObj and msg_type == DATA:
+                fileObj.write(stream)
+            elif fileObj and msg_type == TERMINATION:
+                fileObj.close()
+                fileObj = None
+            else:
+                print("ERROR OCCURED")
